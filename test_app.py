7cd378fbfd817f638171e7f6bfaf62f11162c6e1
mohdhusainahmed001 <mohdhusainahmed001@gmail.com>
2026-07-15 12:47:55 +0530

    updated response.data in  test_app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello from Flask" in response.data
+    assert b"Flask Application is running successfully" in response.data
